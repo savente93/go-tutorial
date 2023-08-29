5a78fb07e4625cfd1dcd5cdf3da806f15c43f747
Sam Vente <savente93@gmail.com>
2023-08-29 09:48:35 +0200

    tick

---
+++ bill.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+func newBill(name string) bill {
+
+	b := bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
+	}
+	return b
+}
+
+func (b bill) format() string {
+	fs := "Bill breakdown: \n"
+	var total float64 = 0
+
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v \n", k+":", v)
+		total += v
+	}
+
+	total += b.tip
+
+	fs += fmt.Sprintf(strings.Repeat("-", 34))
+	fs += fmt.Sprintf("\n")
+	fs += fmt.Sprintf("%-25v ...$%0.2f\n", "tip:", b.tip)
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+
+	return fs
+
+}
+
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+
+}
+
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
